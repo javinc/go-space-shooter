3bef50aebba9417fa22623d55c544003f2884a1c
javinc <javinczki02@gmail.com>
2019-01-31 01:47:40 +0800

    rename cmd folder to game

---
+++ game/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/javinc/ecs"
+	"github.com/javinc/ecs/component"
+	"github.com/javinc/ecs/system"
+	"golang.org/x/image/colornames"
+)
+
+const (
+	title        = "ECS TEST"
+	screenWidth  = 450
+	screenHeight = 600
+)
+
+func main() {
+	engine := ecs.New(title, screenWidth, screenHeight)
+	engine.Start()
+
+	// Register entities.
+	engine.EntityManager.Add(newPlayer())
+	engine.EntityManager.Add(newEnemy())
+
+	// Register systems.
+	engine.SystemManager.Add(system.NewControl(screenWidth, screenHeight))
+	engine.SystemManager.Add(system.NewRender(e.Renderer))
+
+	engine.Run()
+	engine.Stop()
+}
+
+// returns player composition.
+func newPlayer() *ecs.Entity {
+	const size = 20
+	e := ecs.NewEntity()
+	e.Add(component.NewRect(colornames.Red, size, size))
+	// Place player at the bottom-mid of the screen.
+	e.Add(component.NewPosition((screenWidth-size)/2, screenHeight-size))
+	e.Add(component.NewInput())
+	return e
+}
+
+// returns enemy composition.
+func newEnemy() *ecs.Entity {
+	const size = 60
+	e := ecs.NewEntity()
+	e.Add(component.NewRect(colornames.White, size, size))
+	// Placing enemy at the top-mid of the screen.
+	e.Add(component.NewPosition((screenWidth-size)/2, 0))
+	return e
+}
--- system/render.go
+++ system/render.go
@@ -29,18 +29,21 @@ func (s *Render) Process(ee []*ecs.Entity) {
 
 		rect := e.Get("rect").(*component.Rect)
 		pos := e.Get("position").(*component.Position)
-
-		s.r.SetDrawColor(
-			rect.Color.R,
-			rect.Color.G,
-			rect.Color.B,
-			rect.Color.A,
-		)
-		s.r.FillRect(&sdl.Rect{
-			W: rect.W, H: rect.H,
-			X: int32(pos.X), Y: int32(pos.Y),
-		})
+		s.draw(rect, pos)
 	}
 
 	s.r.Present()
+}
+
+func (s *Render) draw(rect *component.Rect, pos *component.Position) {
+	s.r.SetDrawColor(
+		rect.Color.R,
+		rect.Color.G,
+		rect.Color.B,
+		rect.Color.A,
+	)
+	s.r.FillRect(&sdl.Rect{
+		W: rect.W, H: rect.H,
+		X: int32(pos.X), Y: int32(pos.Y),
+	})
 }
